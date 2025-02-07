f6584de743b97a5d54500fdb392616175eef42cd
Emediong Bassey Edem <edememediong1@gmail.com>
2025-02-07 14:30:49 +0100

    end of lesson 2

--- Dave Gray's Typescript/LESSON 2/build/js/main.js
+++ Dave Gray's Typescript/LESSON 2/build/js/main.js
@@ -2,11 +2,16 @@
 var myName = "Emediong";
 var meaningOfLife;
 var isLoading;
-var album;
+var year; //any type
+var album; //union type
 myName = "John";
 meaningOfLife = 42;
 isLoading = true;
-album = 1984;
+year = 1984;
+album = 'Disco';
 var sum = function (a, b) {
     return a + b;
 };
+var postId;
+var isActive;
+var re = /\w+/g;
--- Dave Gray's Typescript/LESSON 2/src/main.ts
+++ Dave Gray's Typescript/LESSON 2/src/main.ts
@@ -1,13 +1,20 @@
 let myName: string = "Emediong"
 let meaningOfLife: number;
 let isLoading: boolean;
-let album: any;
+let year: any;  //any type
+let album: string | number //union type
 
 myName = "John"
 meaningOfLife = 42
 isLoading = true
-album = 1984
+year = 1984
+album = 'Disco'
 
 const sum = (a: number, b: string) => {
     return a + b
 }
+
+let postId: string | number
+let isActive: number | boolean
+
+let re: RegExp = /\w+/g
