29da606b6ee5eb12134200580e5ceb70d4748369
Emediong Bassey Edem <edememediong1@gmail.com>
2025-02-09 17:53:56 +0100

    double assertion

--- Dave Gray's Typescript/LESSON 5/build/js/main.js
+++ Dave Gray's Typescript/LESSON 5/build/js/main.js
@@ -3,3 +3,19 @@
 var a = "hello";
 var b = a; //less Specific
 var c = a; //more specific
+var d = "world";
+var e = "Howdy"; //alternative for an alias
+//the angle brackets are less used in react files (tsx)
+var addOrConcat = function (a, b, c) {
+    if (c === "add")
+        return a + b;
+    return "" + a + b;
+};
+var myVal = addOrConcat(2, 2, "concat");
+// Be careful TS sees no problem - but a string is returned
+var nextVal = addOrConcat(2, 2, "concat");
+//DOUBLE ASSERTION
+//10 as string //Typescript cannot 
+var doubleCast = 10;
+console.log(typeof doubleCast);
+//The DOM
--- Dave Gray's Typescript/LESSON 5/src/main.ts
+++ Dave Gray's Typescript/LESSON 5/src/main.ts
@@ -7,4 +7,23 @@ let a: One = "hello"
 let b = a as Two //less Specific
 let c = a as Three //more specific
 
+let d = <One>"world"
+let e = <string | number>"Howdy"  //alternative for an alias
+//the angle brackets are less used in react files (tsx)
 
+const addOrConcat = (a: number, b: number, c: "add" | "concat"): number | string => {
+    if (c === "add") return a + b
+    return "" + a + b
+}
+
+let myVal:string = addOrConcat(2,2,"concat") as string
+
+// Be careful TS sees no problem - but a string is returned
+let nextVal: number = addOrConcat(2, 2,"concat") as number
+
+//DOUBLE ASSERTION
+//10 as string //Typescript cannot 
+let doubleCast = (10 as unknown) as string
+console.log(typeof doubleCast)
+
+//The DOM
