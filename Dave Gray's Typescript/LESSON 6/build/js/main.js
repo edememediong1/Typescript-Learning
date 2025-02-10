48529b7fb3bf05b1ce0d42be3f18911cef20e455
Emediong Bassey Edem <edememediong1@gmail.com>
2025-02-10 19:05:54 +0100

    class types #1

---
+++ Dave Gray's Typescript/LESSON 6/build/js/main.js
@@ -0,0 +1,10 @@
+"use strict";
+var Coder = /** @class */ (function () {
+    function Coder(name, music, age, lang) {
+        this.name = name;
+        this.music = music;
+        this.age = age;
+        this.lang = lang;
+    }
+    return Coder;
+}());
